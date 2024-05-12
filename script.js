b681e941b9c25ecc2e9fba49f6f793b72f7934c1
Sagar <sagarpatra641@gmail.com>
2024-05-12 20:49:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// <!-- Setup and start animation! -->
+
+var typed = new Typed("#element", {
+  strings: [
+    "Front-end Engineer",
+    "JavaScript Developer",
+    "Web Developer",
+    "React.js Developer",
+  ],
+  typeSpeed: 70,
+  loop: true,
+  loopCount: Infinity,
+});
