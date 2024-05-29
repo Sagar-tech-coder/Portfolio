4bed2d50a0352831d95402315771dced1f0bdd27
Sagar <sagarpatra641@gmail.com>
2024-05-29 13:27:15 +0530

    hamburgur menu added

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ var typed = new Typed("#element", {
   loop: true,
   loopCount: Infinity,
 });
+
+// Hamburger Menu JS Code
+
+const menu_nav = document.querySelector(".mobile-menu");
+
+const navbar = document.querySelector("#navbar");
+
+const toggleNavbar = () => {
+  // alert("Sagar");
+  navbar.classList.toggle("active");
+};
+
+menu_nav.addEventListener("click", () => toggleNavbar());
